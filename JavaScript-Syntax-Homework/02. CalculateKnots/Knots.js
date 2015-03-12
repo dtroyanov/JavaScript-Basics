e1554764254d754fdb0ce6c77f70c7592ccb6cbf
dtroyanov <dtroyanov@yahoo.com>
2015-03-12 10:11:12 +0200

    New homework tasks

---
+++ JavaScript-Syntax-Homework/02. CalculateKnots/Knots.js
@@ -0,0 +1,8 @@
+function calcKnots(number) {
+	var knots = number / 1.852;
+	console.log(knots.toFixed(2));
+}
+
+calcKnots(20);
+calcKnots(112);
+calcKnots(400);
---
+++ JavaScript-Syntax-Homework/03. CylinderVolume/cylinderVol.js
@@ -0,0 +1,8 @@
+﻿function calcCylinderVol(arr) {
+    var radius = arr[0];
+    var height = arr[1];
+    return Math.PI * radius * radius * height;
+}
+console.log(calcCylinderVol([2, 4]).toFixed(3));
+console.log(calcCylinderVol([5, 8]).toFixed(3));
+console.log(calcCylinderVol([12, 3]).toFixed(3));
